c9aa39b706633b5e84eff3587129efefa29beb45
Christiam Artavia <cjafeth89@gmail.com>
2024-03-13 21:20:39 -0600

    modificaciónes escenciales

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("hola mundo");
+
+const texto_nuevo = document.querySelector ("#texto-nuevo");
+const texto_encriptado = document.querySelector ("#texto-encriptado");
+
+console.log(texto_nuevo, texto_encriptado);
