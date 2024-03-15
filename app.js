09ca2c1a623ce1834abfeb58102cc1ae3491e69a
Christiam Artavia <cjafeth89@gmail.com>
2024-03-14 23:09:29 -0600

    ajustes de funcionalidad en js

--- app.js
+++ app.js
@@ -47,3 +47,15 @@ function desencriptar(textoDesencriptado){
     }
     return textoDesencriptado
 }
+function botonCopiar() {
+    // Get the text field
+    var copyText = document.getElementById("texto-encriptado");
+    
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    texto_encriptado.value = "";
+  }
